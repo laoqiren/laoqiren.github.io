75c8d7102aac4bb538a27cd042db1aca96d2154e
laoqiren <@780649403qq.com>
2016-03-12 22:55:22 +0800

    Site updated: 2016-03-12 22:55:21

--- js/main.js
+++ js/main.js
@@ -1,126 +1,151 @@
-(function ($) {
-    // To top button
-    $("#back-to-top").on('click', function () {
-        $('body, html').animate({ scrollTop: 0 }, 600);
-    });
+require([], function (){
 
-    // Nav bar toggle
-    $('#main-nav-toggle').on('click', function () {
-        $('.nav-container-inner').slideToggle();
-    });
-
-    // Caption
-    $('.article-entry').each(function (i) {
-        $(this).find('img').each(function () {
-            if ($(this).parent().hasClass('fancybox')) {
-                return;
-            }
-            var alt = this.alt;
-            if (alt) {
-                $(this).after('<span class="caption">' + alt + '</span>');
-            }
-
-            $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    var isMobileInit = false;
+    var loadMobile = function(){
+        require([yiliaConfig.rootUrl + 'js/mobile.js'], function(mobile){
+            mobile.init();
+            isMobileInit = true;
         });
-
-        $(this).find('.fancybox').each(function(){
-            $(this).attr('rel', 'article' + i);
+    }
+    var isPCInit = false;
+    var loadPC = function(){
+        require([yiliaConfig.rootUrl + 'js/pc.js'], function(pc){
+            pc.init();
+            isPCInit = true;
         });
-    });
-    if ($.fancybox) {
-        $('.fancybox').fancybox();
     }
 
-    // Sidebar expend
-    $('#sidebar .sidebar-toggle').click(function () {
-        if($('#sidebar').hasClass('expend')) {
-            $('#sidebar').removeClass('expend');
-        } else {
-            $('#sidebar').addClass('expend');
+    var browser={
+        versions:function(){
+        var u = window.navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPad: u.indexOf('iPad') > -1, //是否为iPad
+            webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+            };
+        }()
+    }
+
+    $(window).bind("resize", function(){
+        if(isMobileInit && isPCInit){
+            $(window).unbind("resize");
+            return;
+        }
+        var w = $(window).width();
+        if(w >= 700){
+            loadPC();
+        }else{
+            loadMobile();
         }
     });
 
+    if(browser.versions.mobile === true || $(window).width() < 700){
+        loadMobile();
+    }else{
+        loadPC();
+    }
 
-    // Remove extra main nav wrap
-    $('.main-nav-list > li').unwrap();
-
-    // Highlight current nav item
-    $('#main-nav > li > .main-nav-list-link').each(function () {
-        if($('.page-title-link').length > 0){
-            if ($(this).html().toUpperCase() == $('.page-title-link').html().toUpperCase()) {
-                $(this).addClass('current');
-            } else if ($(this).attr('href') == $('.page-title-link').attr('data-url')) {
-                $(this).addClass('current');
-            }
-        }
-    });
-
-    // Auto hide main nav menus
-    function autoHideMenus(){
-        var max_width = $('.nav-container-inner').width() - 10;
-        var main_nav_width = $('#main-nav').width();
-        var sub_nav_width = $('#sub-nav').width();
-        if (main_nav_width + sub_nav_width > max_width) {
-            // If more link not exists
-            if ($('.main-nav-more').length == 0) {
-                $(['<li class="main-nav-list-item top-level-menu main-nav-more">',
-                    '<a class="main-nav-list-link" href="javascript:;">More</a>',
-                    '<ul class="main-nav-list-child">',
-                    '</ul></li>'].join('')).appendTo($('#main-nav'));
-                // Bind hover event
-                $('.main-nav-more').hover(function () {
-                    if($(window).width() < 480) {
-                        return;
+    //是否使用fancybox
+    if(yiliaConfig.fancybox === true){
+        require([yiliaConfig.rootUrl + 'fancybox/jquery.fancybox.js'], function(pc){
+            var isFancy = $(".isFancy");
+            if(isFancy.length != 0){
+                var imgArr = $(".article-inner img");
+                for(var i=0,len=imgArr.length;i<len;i++){
+                    var src = imgArr.eq(i).attr("src");
+                    var title = imgArr.eq(i).attr("alt");
+                    if(typeof(title) == "undefined"){
+                        var title = imgArr.eq(i).attr("title");
                     }
-                    $(this).children('.main-nav-list-child').slideDown('fast');
-                }, function () {
-                    if($(window).width() < 480) {
-                        return;
-                    }
-                    $(this).children('.main-nav-list-child').slideUp('fast');
-                });
-            }
-            var child_count = $('#main-nav').children().length;
-            for (var i = child_count - 2; i >= 0; i--) {
-                var element = $('#main-nav').children().eq(i);
-                if (main_nav_width + sub_nav_width > max_width) {
-                    element.prependTo($('.main-nav-more > ul'));
-                    main_nav_width = $('#main-nav').width();
-                } else {
-                    return;
+                    var width = imgArr.eq(i).attr("width");
+                    var height = imgArr.eq(i).attr("height");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' width="+width+" height="+height+" title='"+title+"' alt='"+title+"'></a>");
                 }
+                $(".article-inner .fancy-ctn").fancybox();
             }
-        }
-        // Nav bar is wide enough
-        if ($('.main-nav-more').length > 0) {
-            $('.main-nav-more > ul').children().appendTo($('#main-nav'));
-            $('.main-nav-more').remove();
-        }
+        });
+
     }
-    autoHideMenus();
+    //是否开启动画
+    if(yiliaConfig.animate === true){
 
-    $(window).resize(function () {
-        autoHideMenus();
-    });
+      if(yiliaConfig.isHome === true) {
+        // 滚动条监听使用scrollreveal.js
+        // https://github.com/jlmakes/scrollreveal.js
+        // 使用cdn[//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js]
+        require([
+          '//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js'
+        ], function (ScrollReveal) {
+          // 更多animation:
+          // http://daneden.github.io/animate.css/
+          var animationNames = [
+            "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+            ],
+            len = animationNames.length,
+            randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
 
-    // Fold second-level menu
-    $('.main-nav-list-item').hover(function () {
-        if ($(window).width() < 480) {
-            return;
-        }
-        $(this).children('.main-nav-list-child').slideDown('fast');
-    }, function () {
-        if ($(window).width() < 480) {
-            return;
-        }
-        $(this).children('.main-nav-list-child').slideUp('fast');
-    });
+          // ie9 不支持css3 keyframe动画, safari不支持requestAnimationFrame, 不使用随机动画，切回原来的动画
+          if (!window.requestAnimationFrame) {
+              $('.body-wrap > article').css({opacity: 1});
 
-    // Add second-level menu mark
-    $('.main-nav-list-item').each(function () {
-        if ($(this).find('.main-nav-list-child').length > 0) {
-            $(this).addClass('top-level-menu');
-        }
-    });
+              if (navigator.userAgent.match(/Safari/i)) {
+                  function showArticle(){
+                      $(".article").each(function(){
+                          if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+                              $(this).removeClass("hidden").addClass("show");
+                              $(this).addClass("is-hiddened");
+                          }else{
+                              if(!$(this).hasClass("is-hiddened")){
+                                  $(this).addClass("hidden");
+                              }
+                          }
+                      });
+                  }
+                  $(window).on('scroll', function(){
+                      showArticle();
+                  });
+                  showArticle();
+              }
+              return;
+          }
+          // document.body有些浏览器不支持监听scroll，所以使用默认的document.documentElement
+          ScrollReveal({
+            duration: 0,
+            afterReveal: function (domEl) {
+              // safari不支持requestAnimationFrame不支持document.documentElement的onscroll所以这里不会执行
+              // 初始状态设为opacity: 0, 动画效果更平滑一些(由于脚本加载是异步，页面元素渲染后在执行动画，感觉像是延时)
+              $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1});
+            }
+          }).reveal('.body-wrap > article');
 
-})(jQuery);
+        });
+      } else {
+        $('.body-wrap > article').css({opacity: 1});
+      }
+
+    }
+
+    //是否新窗口打开链接
+    if(yiliaConfig.open_in_new == true){
+        $(".article a[href]").attr("target", "_blank")
+    }
+    $(".archive-article-title, .github-widget a").attr("target", "_blank");
+
+    //随机颜色
+    var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
+    var id = Math.ceil(Math.random()*(colorList.length-1));
+    //PC
+    $("#container .left-col .overlay").css({"background-color": colorList[id],"opacity": .3});
+    //移动端
+    $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": .7});
+
+    $("table").wrap("<div class='table-area'></div>");
+});
